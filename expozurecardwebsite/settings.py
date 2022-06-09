a48b0f1b5d5eb008f02d22fea6eb5f7a8a551c3b
Danyal Muneer <abbasidani82@gmail.com>
2022-06-09 23:43:16 +0500

    first commit edit

--- expozurecardwebsite/settings.py
+++ expozurecardwebsite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,4 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
